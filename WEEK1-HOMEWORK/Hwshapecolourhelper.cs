6cdf0f59528976f733e0dddca61ebabcf628b13b
05Jinal <jinaldave893@gmail.com>
2025-04-01 15:28:33 +1300

    Completed the homework

---
+++ WEEK1-HOMEWORK/Hwshapecolourhelper.cs
@@ -0,0 +1,25 @@
+﻿
+//Extend the ShapeColourHelper static class so that it can make a shape a random colour from Yellow, Red, Blue, Green or Orange.
+//using Week1ObjectOriented.Interfaces;
+
+namespace Week1ObjectOriented.Classes
+{
+    public static class HwshapeColourHelper
+    {
+
+        public static Hwshape AddColour(Hwshape s)
+        {
+            s.Colour = "Yellow";
+
+            return s;
+        }
+
+        public static Hwshape ChangeColour(Hwshape s)
+        {
+            s.Colour = "Orange";
+
+            return s;
+        }
+
+    }
+}
